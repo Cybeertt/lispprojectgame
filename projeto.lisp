e06dfe362b1e7d516f66451ebf54a3acf900b32a
Cybeertt <rafaelbpalma@gmail.com>
2020-12-21 16:10:08 +0000

    projeto

--- projeto.lisp
+++ projeto.lisp
@@ -1,6 +1,5 @@
 (setq *DEFAULT-PATHNAME-DEFAULTS* (pathname "D:\\LEI\\IA\\20_21\\projeto1\\lispprojectgame\\"))
 
-
 (defun startup ()
   (load "puzzle.lisp" :if-does-not-exist nil)
   (use-package 'operate)
@@ -11,9 +10,5 @@
 
 (startup)
 
-(defun get-problems (file)
-  (with-open-file (s file)
-    (let ((problems nil))
-      (do ((prob (read s) (read s nil 'eof))) ((eq prob 'eof) problems)
-        (setq problems (cons prob problems))))))
+
 
